265b79a8e8ffe9d988517220ae076b11e67991a2
FalkSturmfels <fritzachim2@gmail.com>
2015-12-02 22:41:42 +0100

    Implemented DI Container as Registry

--- core/Registry.php
+++ core/Registry.php
@@ -9,13 +9,112 @@
 class Registry
 {
 
-    private $coreDef = array(
+    private $classMap = array(
         "IDbConnector" => "DbConnector",
         "IDbCommandExecutor" => "DbCommandExecutor",
-        "GetCommand" => "GetCommand"
+        "IGetCommand" => "GetCommand"
     );
 
-    private $coreParams = array(
-        "IDbCommandExecutor" => array("IDbConnector", "GetCommand")
+    private $parameterMap = array(
+        "IDbCommandExecutor" => array("IDbConnector")
     );
+
+    private $singletonList = array(
+        "IDbConnector",
+        "IDbCommandExecutor",
+    );
+
+    private $instanceMap = array();
+
+    public function mapInstance($interfaceName, $className, $paramNames, $isSingleton)
+    {
+        if (!array_key_exists($interfaceName, $this->classMap))
+        {
+            $this->classMap[$interfaceName] = $className;
+
+            if (!empty($paramNames))
+            {
+                $this->parameterMap[$interfaceName] = $paramNames;
+            }
+
+            if ($isSingleton)
+            {
+                array_push($this->singletonList, $interfaceName);
+            }
+        }
+    }
+
+    public function getInstance($interfaceName)
+    {
+        if (array_key_exists($interfaceName, $this->instanceMap))
+        {
+            return $this->instanceMap[$interfaceName];
+        }
+        else
+        {
+            return $this->createInstance($interfaceName);
+        }
+    }
+
+    private function createInstance($interfaceName)
+    {
+        if (array_key_exists($interfaceName, $this->classMap))
+        {
+            $isSingleton = in_array($interfaceName, $this->singletonList);
+            $instance = $this->createCoreInstance($interfaceName);
+
+            if ($isSingleton)
+            {
+                $this->instanceMap[$interfaceName] = $instance;
+            }
+            return $instance;
+        }
+        return null;
+    }
+
+    private function createCoreInstance($interfaceName)
+    {
+        $className = $this->getCoreClass($interfaceName);
+        if ($className !== null)
+        {
+            $paramNames = $this->getCoreParams($interfaceName);
+
+            if ($paramNames !== null)
+            {
+                $params = array();
+                foreach ($paramNames as $name)
+                {
+                    $tmpParam = $this->getInstance($name);
+                    array_push($params, $tmpParam);
+                }
+
+                $reflection = new ReflectionClass($className);
+                $instance = $reflection->newInstanceArgs($params);
+            }
+            else
+            {
+                $instance = new $className();
+            }
+            return $instance;
+        }
+        return null;
+    }
+
+    private function getCoreClass($interfaceName)
+    {
+        if (array_key_exists($interfaceName, $this->classMap))
+        {
+            return $this->classMap[$interfaceName];
+        }
+        return null;
+    }
+
+    private function getCoreParams($interfaceName)
+    {
+        if (array_key_exists($interfaceName, $this->parameterMap))
+        {
+            return $this->parameterMap[$interfaceName];
+        }
+        return null;
+    }
 }
--- test.php
+++ test.php
@@ -12,12 +12,31 @@ require_once "core/Autoloader.php";
 $baseDir = dirname(__FILE__);
 Autoloader::register($baseDir);
 
-$configPath = $baseDir.DIRECTORY_SEPARATOR."main".DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR;
-DbConfig::setConfigFile($configPath."config_db.ini");
+$configPath = $baseDir . DIRECTORY_SEPARATOR . "main" . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR;
+DbConfig::setConfigFile($configPath . "config_db.ini");
 
-$connector = new DbConnector();
+/*$connector = new DbConnector();
 $executor = new DBCommandExecutor($connector);
 $service = new GenericFindService($executor);
-$model = new AttributeDefModel($service);
+$model = new AttributeDefModel($service);*/
 
-var_dump($model->getAttributeDefs());
+/*var_dump($model->getAttributeDefs());*/
+
+$registry = new Registry();
+/*$connector = $registry->getInstance("IGetCommand");
+$connector2 = $registry->getInstance("IGetCommand");*/
+
+$connector = $registry->getInstance("IDbCommandExecutor");
+$connector2 = $registry->getInstance("IDbCommandExecutor");
+
+var_dump($connector);
+var_dump($connector2);
+
+$registry->mapInstance("IAttributeDefFactory", "AttributeDefFactory", null, true);
+$registry->mapInstance("IGenericFindService", "GenericFindService", array("IDbCommandExecutor"), true);
+
+$factory = $registry->getInstance("IAttributeDefFactory");
+$service = $registry->getInstance("IGenericFindService");
+
+var_dump($factory);
+var_dump($service);
