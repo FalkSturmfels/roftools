effdd02484acbc4ef7cec50717197b9cb45f8d9c
FalkSturmfels <fritzachim2@gmail.com>
2015-12-07 21:38:44 +0100

    Moved IGenericFindService to core mapping

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -29,6 +29,7 @@ class Bootstrap
         //$context->mapInstance("IDbConnector", "DbConnector", null, true);
         $context->mapInstance("IDataCommandExecutor", "DataCommandExecutor", array("IDbConnector"), true);
         $context->mapInstance("IGetCommand", "GetCommand", array("IDataCommandExecutor"), false);
+        $context->mapInstance("IGenericFindService", "GenericFindService", array("Registry"), true);
 
         $registry = Registry::getRegistryInstance();
         $registry->addMappingContext($context);
@@ -39,7 +40,6 @@ class Bootstrap
         $context = new MappingContext();
 
         $context->mapInstance("IAttributeDefFactory", "AttributeDefFactory", null, true);
-        $context->mapInstance("IGenericFindService", "GenericFindService", array("Registry"), true);
         $context->mapInstance("IAttributeDefModel", "AttributeDefModel", array("IAttributeDefService"), true);
         $context->mapInstance("IAttributeDefService", "AttributeDefService",
             array("IGenericFindService", "IAttributeDefFactory"), true);
